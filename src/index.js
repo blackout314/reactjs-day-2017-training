06bba413ca8ae7197857c74abe636160dde6e409
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-09-07 22:14:24 +0200

    added logger and dev tools

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import App from './App';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import logger from 'redux-logger'
 import reducers from './redux/reducers'
 
-const store = createStore(reducers)
+const store = createStore(reducers, undefined, compose(
+    applyMiddleware(logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 const app = (
     <Provider store={store}>
